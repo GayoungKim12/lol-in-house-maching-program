96290fc2330018f6565ba4457b34845632d8b192
GayoungKim12 <122119098+GayoungKim12@users.noreply.github.com>
2024-12-21 22:32:35 +0900

    feat: 메인 페이지 구현

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,3 +1,24 @@
+import { Button } from '@/shared/components/ui/button'
+import { Link } from 'react-router-dom'
+
 export default function MainPage() {
-  return <></>
+  return (
+    <div className="flex flex-col mt-36 items-center gap-10 w-full h-full p-10">
+      <div className="flex flex-col justify-center items-center gap-6">
+        <h2 className="font-bold text-6xl">Welcome! ●'◡'●</h2>
+        <div className="flex flex-col justify-center items-center gap-3 font-medium text-xl flex-wrap">
+          <p></p>
+          <p>You can choose between Random Line and Fixed Line.</p>
+        </div>
+      </div>
+      <div className="flex justify-center items-center gap-3">
+        <Button variant="outline" asChild>
+          <Link to={'/matching/random-line'}>라인 랜덤</Link>
+        </Button>
+        <Button variant="outline" asChild>
+          <Link to={'/matching/fixed-line'}>라인 고정</Link>
+        </Button>
+      </div>
+    </div>
+  )
 }
