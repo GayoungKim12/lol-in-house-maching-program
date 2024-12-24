146346ab4b5fc07230e937c2cd1e55ebc09bcb47
GayoungKim12 <122119098+GayoungKim12@users.noreply.github.com>
2024-12-25 04:13:26 +0900

    chore: 텍스트 수정 및 css 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/shared/components/ui/button'
 import { Link } from 'react-router-dom'
+import { Icon } from '@/shared/components/icon'
 
 export default function MainPage() {
   return (
@@ -7,7 +8,7 @@ export default function MainPage() {
       <div className="flex flex-col justify-center items-center gap-6">
         <h2 className="font-bold text-7xl">Welcome! ●'◡'●</h2>
         <div className="flex flex-col justify-center items-center font-medium text-xl flex-wrap">
-          <p>You can choose between Random Line and Fixed Line.</p>
+          <p>You can match teams to play a custom game of LoL with your friends</p>
         </div>
       </div>
       <div className="flex justify-center items-center gap-3">
