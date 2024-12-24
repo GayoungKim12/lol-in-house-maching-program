ad23c7f2cbd72adccc897bc14827772f541fd6b1
GayoungKim12 <122119098+GayoungKim12@users.noreply.github.com>
2024-12-25 01:33:37 +0900

    rename: 타입명 변경

--- src/features/matching-team/stores/usePairsStore.ts
+++ src/features/matching-team/stores/usePairsStore.ts
@@ -1,13 +1,13 @@
 import { create } from 'zustand'
 import { Pair } from '@/shared/types/teamRole'
 
-interface PlayersStore {
+interface PairsStore {
   pairs: Pair[]
 
   setPairs: (pairs: Pair[]) => void
 }
 
-const usePairsStore = create<PlayersStore>((set) => ({
+const usePairsStore = create<PairsStore>((set) => ({
   pairs: Array(5).fill(0).map(() => ({ player1: '', player2: '' })),
 
   setPairs: (pairs: Pair[]) => set({ pairs }),
