d2375144b020dfd78b3bec9f12f935cc7a99c455
GayoungKim12 <122119098+GayoungKim12@users.noreply.github.com>
2024-12-25 03:22:13 +0900

    chore: css 수정

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="h-12 flex gap-4 items-center px-4">
       <SidebarTrigger />
-      <h1 className="font-bold text-xl">
+      <h1 className="font-bold text-xl hover:opacity-60">
         <Link to="/">
           LoL Matching
         </Link>
