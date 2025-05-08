eb94046a69c4c84a312d954bd6713b409e4eec02
GayoungKim12 <122119098+GayoungKim12@users.noreply.github.com>
2025-05-08 10:32:19 +0900

    chore: 환경변수 추가

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,8 +1,10 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_RIOT_API_BASE_URL: string;
   readonly VITE_RIOT_API_KEY: string;
+  readonly VITE_RIOT_ASIA_API_URL: string;
+  readonly VITE_RIOT_KR_API_URL: string;
+  readonly VITE_RIOT_PROFILE_ICON_URL: string;
 }
 
 interface ImportMeta {
