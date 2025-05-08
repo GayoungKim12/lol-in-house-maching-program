fa937cccf8c02d3d1fc9d2d595d80932a9a75b36
GayoungKim12 <122119098+GayoungKim12@users.noreply.github.com>
2025-05-08 17:26:46 +0900

    feat: 소환사 정보 및 랭크 정보 api 훅 적용

--- src/features/searching-user/ui/UserProfile.tsx
+++ src/features/searching-user/ui/UserProfile.tsx
@@ -1,39 +1,43 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/shared/components/ui/card'
 import { Badge } from '@/shared/components/ui/badge'
-import { SummonerProfile } from '@/features/searching-user/hooks/useGetSearchedUsers'
 import getTierColor from '@/shared/lib/utils/getTierColor'
+import useGetLeagueEntry from '@/features/searching-user/hooks/useGetLeagueEntry'
+import useGetSummoner from '@/features/searching-user/hooks/useGetSummoner'
+import useUserStore from '@/features/searching-user/store/useUserStore'
 
-interface UserProfileProps {
-  searchedUser: SummonerProfile
-}
+export default function UserProfile() {
+  const { gameName, tagLine } = useUserStore()
+  const { data: summoner } = useGetSummoner()
+  const { data: leagueEntry } = useGetLeagueEntry()
 
-export default function UserProfile({ searchedUser }: UserProfileProps) {
+  if (!(summoner && leagueEntry && 'puuid' in summoner && leagueEntry instanceof Array)) return null
+
   return (
     <Card className="flex flex-row px-7 items-center">
       <div className="relative w-32 bottom-1">
         <img
-          src={`${import.meta.env.VITE_RIOT_PROFILE_ICON_URL}/${searchedUser.profileIconId}.jpg`}
+          src={`${import.meta.env.VITE_RIOT_PROFILE_ICON_URL}/${summoner.profileIconId}.jpg`}
           alt="프로필 아이콘"
           className="w-full rounded-3xl"
         />
         <Badge className="absolute left-1/2  -translate-x-1/2 z-10 -translate-y-1/2 bg-black">
-          {searchedUser.summonerLevel}
+          {summoner.summonerLevel}
         </Badge>
       </div>
       <div>
         <CardHeader className="flex flex-row items-center space-y-0 pb-3">
           <CardTitle>
-            {searchedUser.account.gameName}
-            <span className="ml-1 text-lg text-gray-500 text font-medium">#{searchedUser.account.tagLine}</span>
+            {gameName}
+            <span className="ml-1 text-lg text-gray-500 text font-medium">#{tagLine}</span>
           </CardTitle>
         </CardHeader>
         <CardContent className="pb-7 pl-7">
           {/* 소환사 랭크 정보 */}
           <div className="space-y-2">
             <h3 className="text-sm font-medium">랭크 정보</h3>
-            {searchedUser.leagueEntries && searchedUser.leagueEntries.length > 0 ? (
+            {leagueEntry ? (
               <div className="flex flex-wrap gap-3">
-                {searchedUser.leagueEntries.map((entry, index) => (
+                {leagueEntry.map((entry, index) => (
                   <div key={index} className="flex flex-col">
                   <span className="text-xs text-gray-500">
                     {entry.queueType === 'RANKED_SOLO_5x5' ? '솔로랭크' : entry.queueType === 'RANKED_FLEX_SR' ? '자유랭크' : entry.queueType}
