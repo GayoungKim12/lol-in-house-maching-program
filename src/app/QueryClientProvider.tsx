21bd52b3af5b4ffa332b832c44fb428e110f6d65
GayoungKim12 <122119098+GayoungKim12@users.noreply.github.com>
2025-05-09 10:57:15 +0900

    feat: api 에러 발생 시 자동으로 토스트 팝업 뜨도록 함

--- src/app/QueryClientProvider.tsx
+++ src/app/QueryClientProvider.tsx
@@ -2,8 +2,21 @@
 
 import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode } from 'react'
+import { useToast } from '@/shared/hooks/use-toast'
 
 export default function QueryProvider({ children }: { children: ReactNode }) {
+  const { toast } = useToast()
+
+  const handleError = (error: Error) => {
+    const [title, description] = error.message.split('|')
+
+    toast({
+      variant: 'destructive',
+      title: title || '오류',
+      description: description || '알 수 없는 에러가 발생했습니다.',
+    })
+  }
+
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -12,8 +25,12 @@ export default function QueryProvider({ children }: { children: ReactNode }) {
         retry: 1,
       },
     },
-    queryCache: new QueryCache(),
-    mutationCache: new MutationCache(),
+    queryCache: new QueryCache({
+      onError: handleError,
+    }),
+    mutationCache: new MutationCache({
+      onError: handleError,
+    }),
   })
 
   return (
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,13 @@
 import AppRouter from '@/app/route'
 import { BrowserRouter } from 'react-router-dom'
 import Provider from '@/app/provider'
+import { Toaster } from '@/shared/components/ui/toaster'
 
 export default function App() {
   return (
     <Provider>
       <BrowserRouter>
+        <Toaster />
         <AppRouter />
       </BrowserRouter>
     </Provider>
