01ee5f6419289c7bc2f625610cf8ce4942e4dd57
GayoungKim12 <122119098+GayoungKim12@users.noreply.github.com>
2025-05-09 14:37:11 +0900

    chore: 프로필 아이콘 url 변경

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -4,7 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_RIOT_API_KEY: string;
   readonly VITE_RIOT_ASIA_API_URL: string;
   readonly VITE_RIOT_KR_API_URL: string;
-  readonly VITE_RIOT_PROFILE_ICON_URL: string;
+  readonly VITE_RIOT_ICON_URL: string;
 }
 
 interface ImportMeta {
