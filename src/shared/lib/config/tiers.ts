2e8d165ea70fe3ecf168c0e795a1fcdd3b8f2615
GayoungKim12 <122119098+GayoungKim12@users.noreply.github.com>
2025-05-09 11:21:43 +0900

    refactor: 티어색 config 분리

---
+++ src/shared/lib/config/tiers.ts
@@ -0,0 +1,12 @@
+export const tiers: Record<string, string> = {
+  IRON: 'bg-gray-400',
+  BRONZE: 'bg-amber-800',
+  SILVER: 'bg-gray-300',
+  GOLD: 'bg-amber-400',
+  PLATINUM: 'bg-teal-500',
+  EMERALD: 'bg-emerald-500',
+  DIAMOND: 'bg-blue-500',
+  MASTER: 'bg-purple-600',
+  GRANDMASTER: 'bg-red-600',
+  CHALLENGER: 'bg-amber-200 text-cyan-500',
+}
--- src/shared/lib/utils/getTierColor.ts
+++ src/shared/lib/utils/getTierColor.ts
@@ -1,15 +1,4 @@
-const tiers: Record<string, string> = {
-  IRON: 'bg-gray-400',
-  BRONZE: 'bg-amber-800',
-  SILVER: 'bg-gray-300',
-  GOLD: 'bg-amber-400',
-  PLATINUM: 'bg-teal-500',
-  EMERALD: 'bg-emerald-500',
-  DIAMOND: 'bg-blue-500',
-  MASTER: 'bg-purple-600',
-  GRANDMASTER: 'bg-red-600',
-  CHALLENGER: 'bg-yellow-300 text-black',
-}
+import { tiers } from '@/shared/lib/config/tiers'
 
 export default function getTierColor(tier?: string) {
   return tier ? tiers[tier] || 'bg-gray-500' : 'bg-gray-500'
