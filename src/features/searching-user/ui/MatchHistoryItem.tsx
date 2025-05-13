253cbd025f6bfb3756cd2ae3bde125dd06a7f48d
GayoungKim12 <122119098+GayoungKim12@users.noreply.github.com>
2025-05-13 15:01:12 +0900

    refactor: 매칭 플레이어 리팩토링

--- src/features/searching-user/ui/MatchHistoryItem.tsx
+++ src/features/searching-user/ui/MatchHistoryItem.tsx
@@ -8,7 +8,7 @@ import { Icon } from '@/shared/components/icon'
 import GameInfo from '@/features/searching-user/ui/GameInfo'
 import ChampionAndKDA from '@/features/searching-user/ui/ChampionAndKDA'
 import ItemList from '@/features/searching-user/ui/ItemList'
-import { Team } from '@/shared/lib/config/gameOptions'
+import MatchPlayer from '@/features/searching-user/ui/MatchPlayers'
 
 interface MatchHistoryItemProps {
   match: MatchInfo;
@@ -20,20 +20,8 @@ interface MatchHistoryItemProps {
 export default function MatchHistoryItem({ match }: MatchHistoryItemProps) {
   const { data: riotAccount } = useGetRiotAccount()
   const [isOpen, setIsOpen] = useState(false) // 상세 정보 토글 상태
-
-  const blueTeam = match.info.participants.filter((p) => p.teamId === Team.BLUE)
-  const redTeam = match.info.participants.filter((p) => p.teamId === Team.RED)
-
-  const teams = [{
-    label: '블루팀',
-    players: blueTeam,
-  }, {
-    label: '레드팀',
-    players: redTeam,
-  }]
-
-  // 현재 사용자 정보 찾기
   const player = match.info.participants.find((p) => p.puuid === riotAccount?.puuid)
+
   const handleClick = (e: MouseEvent) => {
     e.preventDefault() // Link 이동 방지
     setIsOpen((prev) => !prev)
@@ -56,23 +44,7 @@ export default function MatchHistoryItem({ match }: MatchHistoryItemProps) {
           </div>
 
           <div className="flex items-end md:w-full">
-            <div className="hidden md:flex ml-2 w-full">
-              {teams.map(team => (
-                <ul className="flex flex-col gap-0.5 mr-4">
-                  {team.players.map(player => (
-                    <li className="flex text-xs truncate">
-                      <div className="w-4 h-4 rounded-sm overflow-hidden mr-2">
-                        <img src={`${import.meta.env.VITE_RIOT_ICON_URL}/v1/champion-icons/${player.championId}.png`}
-                             alt={player.championName} className="w-full h-full aspect-square scale-110" />
-                      </div>
-                      <span className="max-w-32 truncate">
-                      {player.riotIdGameName + '#' + player.riotIdTagline}
-                      </span>
-                    </li>
-                  ))}
-                </ul>
-              ))}
-            </div>
+            <MatchPlayer match={match} />
 
             <button className="inline-flex items-center justify-center w-8 h-8 text-gray-500 hover:text-black"
                     onClick={handleClick}
---
+++ src/features/searching-user/ui/MatchPlayers.tsx
@@ -0,0 +1,39 @@
+import { Team } from '@/shared/lib/config/gameOptions'
+import { MatchInfo } from '@/features/searching-user/utils/apiGetMatch'
+
+interface MatchPlayerProps {
+  match: MatchInfo
+}
+
+export default function MatchPlayer({ match }: MatchPlayerProps) {
+  const blueTeam = match.info.participants.filter((p) => p.teamId === Team.BLUE)
+  const redTeam = match.info.participants.filter((p) => p.teamId === Team.RED)
+
+  const teams = [{
+    label: '블루팀',
+    players: blueTeam,
+  }, {
+    label: '레드팀',
+    players: redTeam,
+  }]
+
+  return (
+    <div className="hidden md:flex ml-2 w-full">
+      {teams.map(team => (
+        <ul className="flex flex-col gap-0.5 mr-4">
+          {team.players.map(player => (
+            <li className="flex text-xs truncate">
+              <div className="w-4 h-4 rounded-sm overflow-hidden mr-2">
+                <img src={`${import.meta.env.VITE_RIOT_ICON_URL}/v1/champion-icons/${player.championId}.png`}
+                     alt={player.championName} className="w-full h-full aspect-square scale-110" />
+              </div>
+              <span className="max-w-32 truncate">
+                      {player.riotIdGameName + '#' + player.riotIdTagline}
+                      </span>
+            </li>
+          ))}
+        </ul>
+      ))}
+    </div>
+  )
+}
