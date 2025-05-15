fb1c2786fffde1afcf069030f66e032cbb1c1481
GayoungKim12 <122119098+GayoungKim12@users.noreply.github.com>
2025-05-15 11:52:20 +0900

    chore: supabase 설치 및 설정

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,12 +1,14 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_RIOT_API_KEY: string;
-  readonly VITE_RIOT_ASIA_API_URL: string;
-  readonly VITE_RIOT_KR_API_URL: string;
-  readonly VITE_RIOT_ICON_URL: string;
-  readonly VITE_DDRAGON_CHAMPION_URL: string;
-  readonly VITE_DDRAGON_ITEM_URL: string;
+  readonly VITE_RIOT_API_KEY: string
+  readonly VITE_RIOT_ASIA_API_URL: string
+  readonly VITE_RIOT_KR_API_URL: string
+  readonly VITE_RIOT_ICON_URL: string
+  readonly VITE_DDRAGON_CHAMPION_URL: string
+  readonly VITE_DDRAGON_ITEM_URL: string
+  readonly VITE_PUBLIC_SUPABASE_URL: string
+  readonly VITE_PUBLIC_SUPABASE_ANON_KEY: string
 }
 
 interface ImportMeta {
