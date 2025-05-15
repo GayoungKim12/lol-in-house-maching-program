71fec8fb5d0c502af5b04d8f6f58472185271575
GayoungKim12 <122119098+GayoungKim12@users.noreply.github.com>
2025-05-15 14:02:06 +0900

    feat: supabase api 함수 형식 변경

--- src/features/searching-user/utils/apiGetCustomMatchIds.ts
+++ src/features/searching-user/utils/apiGetCustomMatchIds.ts
@@ -2,13 +2,14 @@ import { supabase } from '@/shared/lib/supabase/supabaseClient'
 import handleAPIError from '@/shared/lib/utils/handleAPIError'
 
 export default async function apiGetCustomMatchIds(puuid: string): Promise<string[] | undefined> {
-  try {
-    const { data } = await supabase.from('custom_match_player')
-      .select('match_id')
-      .eq('puuid', puuid)
+  const { data, error } = await supabase.from('custom_match_player')
+    .select('match_id')
+    .eq('puuid', puuid)
 
-    return data?.map(({ match_id }) => match_id)
-  } catch {
+  if (error) {
     handleAPIError('NOT_FOUND_MATCH_HISTORY')
   }
+
+  return data?.map(({ match_id }) => match_id)
+
 }
